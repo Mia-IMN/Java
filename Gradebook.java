b2b44ef6be72fe7bf2e6da4cd656053c1e48331b
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-04-18 14:26:14 +0100

    Updated Gradebook

--- Gradebook.java
+++ Gradebook.java
@@ -15,6 +15,20 @@ public class Gradebook {
     }
 
     public void displayMessage(){
-        
+        System.out.printf("This is the gradebook for \n%s!\n\n", getCourseName());
     }
+}
+
+public void ClassAverage(){
+    Scanner input = new Scanner(System.in);
+    int total = 0, counter = 1, grade, Average;
+    while (counter <= 10){
+        System.out.println("Enter Grade");
+        grade = input.nextInt();
+        total = total + grade;
+        counter = counter + 1;
+    }
+    Average = (total/10);
+    System.out.printf("\nTotal of all 10 grades is %d\n", total);
+    System.out.printf("\nAverage is %d\n", Average);
 }
