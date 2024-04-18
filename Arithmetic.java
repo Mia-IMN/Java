45a3364d615291649c329833aed52dc3f01e2fb1
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-04-18 18:42:30 +0100

    Update

---
+++ Arithmetic.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Arithmetic {
+    public static void main(String args[]){
+        int a, b, c;
+        int sum, smallest, largest;
+        double average, product;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter first number a: ");
+        a = in.nextInt();
+        System.out.println("Enter second number b: ");
+        b = in.nextInt();
+        System.out.println("Enter third number c: ");
+        c = in.nextInt();
+
+        // sum = a + b + c;
+        // if (a > b && b > c){
+        //         largest = a;
+        //         smallest = c;
+        //         System.out.println("");
+        //     }
+        // else if(b > a s)
+        // }
+    }
+}
