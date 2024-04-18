6b0343f2d211d16fe8909a9a9e2281f2e6f46f14
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-04-18 13:38:22 +0100

    Gradebook

---
+++ Gradebook.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Gradebook {
+    private String CourseName;
+
+    public Gradebook(String name){
+        CourseName = name;
+    }
+
+    public void setCourseName(String name){
+        CourseName = name;
+    }
+
+    public String getCourseName(){
+        return CourseName;
+    }
+
+    public void displayMessage(){
+        
+    }
+}
