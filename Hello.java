748e7fa64fd14287eccb9f638191ae096e742fa5
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-04-19 18:51:55 +0100

    Modified hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 public class Hello {
     public static void main(String args[]){
-        
+        String name;
+        Scanner input = new Scanner(System.in);
+        name = input.nextLine();
+        System.out.printf("Hi %s!", name);   
     }
 }
