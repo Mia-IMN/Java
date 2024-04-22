9c4d26419921d46bb6e20808038e9e0861dca312
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-04-22 21:55:20 +0100

    Fixed bug

--- Hello.java
+++ Hello.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 public class Hello {
     public static void main(String args[]){
-        String name = "Miracle";
-        // Scanner input = new Scanner(System.in);
-        // name = new String(input.nextLine());
+        String name;
+        Scanner input = new Scanner(System.in);
+        name = new String(input.nextLine());
         System.out.printf("Hi %s!", name);   
     }
 }
