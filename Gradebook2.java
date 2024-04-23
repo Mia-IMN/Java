92d1c63b7d489940e141523245b6d84fd4e77fd8
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-04-23 02:49:12 +0100

    code test

---
+++ Gradebook2.java
@@ -0,0 +1,11 @@
+public class Gradebook2 {
+    
+    public void displayMessage(){
+        System.out.println("Welcome to Gradebook");
+    }
+
+    public static void main(String args[]){
+        Gradebook2 myGradebook = new Gradebook2();
+        myGradebook.displayMessage();
+    }
+}
