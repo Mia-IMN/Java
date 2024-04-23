2143d00760f2e79760ce9c02846291db46065b58
Mia-IMN <mira.ikechukwu15@gmail.com>
2024-04-23 01:54:27 +0100

    New file Add.java

---
+++ Add.java
@@ -0,0 +1,7 @@
+public class Add{
+    public static int num1 = 3, num2 = 5, sum;
+    public static void main(String args[]){
+        sum = num1 + num2;
+        System.out.println("The sum is " + sum);
+    }
+}
